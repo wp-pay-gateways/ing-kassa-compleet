c040d2660bbe91fada49b3ed82555aeb8d63a515
Remco Tolsma <info@remcotolsma.nl>
2015-11-04 14:20:24 +0100

    Fixed namespace.

--- src/PaymentMethods.php
+++ src/PaymentMethods.php
@@ -8,7 +8,7 @@
  * @author Remco Tolsma
  * @version 1.0.0
  */
-class Pronamic_WP_Pay_ING_KassaCompleet_PaymentMethods {
+class Pronamic_WP_Pay_Gateways_ING_KassaCompleet_PaymentMethods {
 	/**
 	 * Constant for the iDEAL payment method.
 	 *
--- src/PaymentMethodsHelper.php
+++ src/PaymentMethodsHelper.php
@@ -8,7 +8,7 @@
  * @author Remco Tolsma
  * @version 1.0.0
  */
-class Pronamic_WP_Pay_ING_KassaCompleet_PaymentMethodsHelper {
+class Pronamic_WP_Pay_Gateways_ING_KassaCompleet_PaymentMethodsHelper {
 	/**
 	 * Transform an ING Kassa Compleet payment method to the WordPress pay payment method.
 	 *
