5cc32fa049e2a38852f0b190a9f3efbf30c526fa
Remco Tolsma <info@remcotolsma.nl>
2015-11-11 15:09:39 +0100

    Added an integration class.

---
+++ src/Integration.php
@@ -0,0 +1,26 @@
+<?php
+
+class Pronamic_WP_Pay_Gateways_ING_KassaCompleet_Integration {
+	public function __construct() {
+		$this->id            = 'ing-kassa-compleet';
+		$this->name          = 'ING Kassa Compleet';
+		$this->provider      = 'ing';
+		$this->dashboard_url = 'https://portal.kassacompleet.nl/';
+	}
+
+	public function get_config_factory_class() {
+		
+	}
+
+	public function get_config_class() {
+		
+	}
+
+	public function get_settings_class() {
+		return 'Pronamic_WP_Pay_Gateways_ING_KassaCompleet_Settings';
+	}
+
+	public function get_gateway_class() {
+		
+	}
+}
