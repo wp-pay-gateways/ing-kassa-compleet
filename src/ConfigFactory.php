d6150585e697e8dcb230a05b44c12cfe75359330
Reüel van der Steege <reuel@pronamic.nl>
2018-01-17 12:02:35 +0100

    Happy 2018.

--- src/ConfigFactory.php
+++ src/ConfigFactory.php
@@ -3,7 +3,7 @@
 /**
  * Title: ING Kassa Compleet config factory
  * Description:
- * Copyright: Copyright (c) 2005 - 2017
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Reüel van der Steege
